c5c00a00c0afcc24a725928970f1f5a53c05beea
Suryadheeraz <56762407+Suryadheeraz@users.noreply.github.com>
2025-11-07 16:37:23 +0530

    Removing

--- truffle/migrations/1_deploy_casino.js
+++
@@ -1,6 +0,0 @@
-var Casino = artifacts.require("Casino");
-
-module.exports = async function(deployer,accounts) {
-    await deployer.deploy(Casino);
-    console.log("Owners account = ", accounts[0]);
- }
